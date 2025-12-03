14b763b8d50e6e411b25e34202ead6945627b6bd
sguav <only1sguav@gmail.com>
2025-12-03 11:11:35 +0100

    [2025] change day 03 approach for parametric string...

--- 2025/03/joltage.py
+++ 2025/03/joltage.py
@@ -10,56 +10,41 @@ def debugprint(*args, **kwargs):
     if DEBUG:
         print(*args, **kwargs)
 
-global joltage_accumulator
-
 def max_substring(sub_bank: str):
     max_val = max(sub_bank)
     return max_val, sub_bank.index(max_val)+1
 
-def largest_joltage(bank: str) -> int:
+def largest_joltage(bank: str, num_batteries=2) -> int:
     """
     Given a string of digits (battery joltage bank)
-    Return the highest 2 digits number that can be
+    Return the highest 'num_batteries'-digits number that can be
     found in string order
     """
+    joltage = 0
 
-    d, i = max_substring(bank[:-1])
-    first_digit = d
-    first_index = i
-    second_digit = 0
+    print(f"> '{bank}' ", end="| ")
 
-    print(f"Bank: {bank} - Initial: {first_digit}(bank[{first_index}])")
-    # print(f"Bank: {bank} - Initial: {first_digit}{second_digit}")
+    for d in range(num_batteries - 1, -1, -1): # Iterate digit from max number
+        max_battery = max(bank[:-d] if d >= 1 else bank) # get the highest digit in the truncated (or full) substring
+        bank = bank[bank.index(max_battery) + 1:] # Reduce substring
+        joltage += int(max_battery) * (10 ** d)
 
-    while i < len(bank):
-        d, ii = max_substring(bank[i:])
-        i += ii
-        d = int(d)
-        if i < len(bank[i:-1]):
-            if d > int(first_digit):
-                first_digit = str(d)
-            elif d > int(second_digit):
-                second_digit = str(d)
-        else:
-            second_digit = str(d) if d > int(second_digit) else second_digit
-
-    print(f"Highest joltage in this bank: {first_digit}{second_digit}")
-    return int(first_digit+second_digit)
-
-
-
+    print(f"Highest joltage ({num_batteries} digits) in this bank: {joltage}")
+    return int(joltage)
 
 def main():
-    global joltage_accumulator
-    joltage_accumulator = 0
+    joltage_accumulator_1 = 0
+    joltage_accumulator_2 = 0
 
     input_file = "test" if (TEST) else "input"
     with open(f'{SCRIPT_PATH}/{input_file}') as f:
         while bank := f.readline():
-            joltage_accumulator += largest_joltage(bank[:-1]) # Strip newline
+            joltage_accumulator_1 += largest_joltage(bank[:-1]) # Strip newline
+            joltage_accumulator_2 += largest_joltage(bank[:-1],12) # Strip newline
 
 
-    print(f"The accumulated joltage is: {joltage_accumulator}")
+    print(f"The accumulated joltage for part 1 is: {joltage_accumulator_1}")
+    print(f"The accumulated joltage for part 2 is: {joltage_accumulator_2}")
     # Part 1: 17278
 
 if __name__ == '__main__':
